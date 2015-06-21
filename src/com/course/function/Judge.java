74c8264f0daa181d494136da5957e4aa4f60f292
peng45588 <913704951@qq.com>
2015-06-21 20:09:43 +0800

    add judge function

--- src/com/course/function/Judge.java
+++ src/com/course/function/Judge.java
@@ -18,7 +18,7 @@ public class Judge {
     final static DataAccessInterface<SchoolInfo> dacSch = DACFactory.getInstance().createDAC(SchoolInfo.class);
     final static DataAccessInterface<StudentInfo> dacStu = DACFactory.getInstance().createDAC(StudentInfo.class);
     final static DataAccessInterface<CourseInfo> dacCou = DACFactory.getInstance().createDAC(CourseInfo.class);
-    //判断学院是否存在
+    //  学院不存在 false
     public static boolean isSchool(String schoolName){
 
         Condition<SchoolInfo> conditionSch = new Condition<SchoolInfo>() {
@@ -49,7 +49,7 @@ public class Judge {
             return true;
     }
 
-    // 课程不存在 false
+    //  课程不存在 false
     public static boolean isCourse(String courseId){
         Condition<CourseInfo> conditionCou = new Condition<CourseInfo>() {
             @Override
@@ -64,18 +64,134 @@ public class Judge {
             return true;
     }
 
-    //TODO 学分已满 false
+    //录入课程  时间地点唯一 不唯一（冲突）：false
+    public static boolean isTimeOrLocation(Time time,String location){
+        //选择所有课程
+        Condition<CourseInfo> conditionCou = new Condition<CourseInfo>() {
+            @Override
+            public boolean assertBean(CourseInfo courseInfo) {
+                if(courseInfo.getTime().getWeekday()==time.getWeekday()&&
+                        courseInfo.getTime().getPeriod()==time.getPeriod())
+                    if (courseInfo.getLocation().equals(location))
+                        return true;
+                return false;
+            }
+        };
+        List list = dacCou.selectByCondition(conditionCou);
+        if (list.size()==0)
+            return true;
+        else
+            return false;
+    }
+
+    //录入课程 教师时间是否可选  不可选（冲突）: false
+    public static boolean isTeacherTime(Time time,String teacherName){
+        Condition<CourseInfo> conditionCou = new Condition<CourseInfo>() {
+            @Override
+            public boolean assertBean(CourseInfo courseInfo) {
+                if(courseInfo.getTime().getWeekday()==time.getWeekday()&&
+                        courseInfo.getTime().getPeriod()==time.getPeriod())
+                    if (courseInfo.getTeacherName().equals(teacherName))
+                        return true;
+                return false;
+            }
+        };
+        List list = dacCou.selectByCondition(conditionCou);
+        if (list.size()==0)
+            return true;
+        else
+            return false;
+    }
+
+    //录入学生：学号唯一  不唯一（冲突）：false
+    public static boolean isStudentId(String studentId){
+        Condition<StudentInfo> conditionStu = new Condition<StudentInfo>() {
+            @Override
+            public boolean assertBean(StudentInfo studentInfo) {
+                if (studentInfo.getStudentId().equals(studentId))
+                    return true;
+                return false;
+            }
+        };
+        List list = dacStu.selectByCondition(conditionStu);
+        if (list.size()==0)
+            return true;
+        else
+            return false;
+    }
+
+    //选课   时间地点是否唯一 冲突：false
+    public static boolean isSelectTimeOrLocation(String studentId,String courseId){
+        Condition<StudentInfo> conditionStu = new Condition<StudentInfo>() {
+            @Override
+            public boolean assertBean(StudentInfo studentInfo) {
+                return studentInfo.getStudentId().equals(studentId);
+            }
+        };
+        List<StudentInfo> list = dacStu.selectByCondition(conditionStu);
+        List<String> stuCourseId = list.get(0).getCourseId();
+
+        //获得学生已选课程
+        Condition<CourseInfo> conditionCou = new Condition<CourseInfo>() {
+            @Override
+            public boolean assertBean(CourseInfo courseInfo) {
+                if (stuCourseId.contains(courseInfo.getCourseId()))
+                    return true;
+                return false;
+            }
+        };
+        List<CourseInfo> listCou = dacCou.selectByCondition(conditionCou);
+
+        //获得所要选课程
+        conditionCou = new Condition<CourseInfo>() {
+            @Override
+            public boolean assertBean(CourseInfo courseInfo) {
+                return courseInfo.getCourseId().equals(courseId);
+            }
+        };
+        CourseInfo course = (CourseInfo) dacCou.selectByCondition(conditionCou);
+        //判断时间地点是否冲突
+        for (int i=0;i<listCou.size();i++) {
+            if (listCou.get(i).getLocation().equals(course.getLocation()))
+                if (listCou.get(i).getTime().getPeriod()==course.getTime().getPeriod()&&
+                        listCou.get(i).getTime().getWeekday()==course.getTime().getWeekday())
+                    return false;
+        }
+        return true;
+    }
+
+    //TODO 学分   已满 false
     public static boolean isCredit(){
         return false;
     }
 
-    //TODO 选课时间地点冲
-    public static boolean isTimeOrLocation(Time time,String location){
-        return true;
+    // 选课人数已满
+    public static boolean isSelectFull(String courseId){
+        Condition<CourseInfo> conditionCou = new Condition<CourseInfo>() {
+            @Override
+            public boolean assertBean(CourseInfo courseInfo) {
+                return courseInfo.getCourseId().equals(courseId);
+            }
+        };
+        List<CourseInfo> list = dacCou.selectByCondition(conditionCou);
+        if (list.get(0).getSurplus()>0)
+            return true;
+        return false;
     }
 
-    // TODO 选课人数已满
-    public static boolean isFull(){
-        return false;
+    //学生未选课 已选：true
+    public static boolean isSelect(String studentId,String courseId){
+        Condition<StudentInfo> conditionStu = new Condition<StudentInfo>() {
+            @Override
+            public boolean assertBean(StudentInfo studentInfo) {
+                return studentInfo.getStudentId().equals(studentId);
+            }
+        };
+        StudentInfo stu = dacStu.selectByCondition(conditionStu).get(0);
+        List<String> course = stu.getCourseId();
+        if (course.contains(courseId))
+            return true;
+        else
+            return false;
     }
 }
